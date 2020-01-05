e4ab096becb19a530a8f48ea4a2b0b76018f7671
Panayiotis Nicolaou <p.nicolaou.13@gmail.com>
2020-01-05 15:47:26 +0200

    Add tests for ItemsStep

---
+++ packages/flavorli-webapp/src/StepByStep/components/ItemsStep.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {render} from '../../helpers/test-helpers';
+import {axe} from 'jest-axe';
+import ItemsStep from './ItemsStep';
+import {items} from '../helpers/mockData';
+import {StepByStepProvider} from '../helpers/StepByStepContext';
+
+const setup = () => {
+  return render(
+    <>
+      {/* Add a div with id recipe-steps to be used by aria-controls */}
+      <div id="recipe-steps" />
+      <StepByStepProvider initialValues={{noOfSteps: 10}}>
+        <ItemsStep items={items} />
+      </StepByStepProvider>
+    </>,
+  );
+};
+
+describe('ItemsStep', () => {
+  it('should not have any axe violations', async () => {
+    const {container} = setup();
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it('should render correctly', () => {
+    const {container} = setup();
+    expect(container).toMatchSnapshot();
+  });
+});
